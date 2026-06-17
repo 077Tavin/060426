b35bb749e32d2f3f0381948619f945766c02adec
077Tavin <otavio.goncalves.cruz@escola.pr.gov.br>
2026-06-17 08:48:52 -0300

    ////

--- main.js
+++ main.js
@@ -1,66 +1,45 @@
 
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
     for (let j = 0; j < botoes.length; j++) {
       botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
     }
 
     botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
   };
 }
+
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-08-18T15:37:07");
+const tempoObjetivo1 = new Date("2026-11-30");
 
-contadores[0].textContent = tempoObjetivo1;
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-08-18T15:37:07")
-
-let tempo = tempoObjetivo1.getTime();
-
-contadores[0].textContent = tempo;
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-08-18T15:37:07")
-
-let tempo = tempoObjetivo1.getMonth();
-
-contadores[0].textContent = tempo;
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-08-18T15:37:07")
-
-let tempo = tempoObjetivo1.getFullYear();
-
-contadores[0].textContent = tempo;
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
 function calculaTempo(tempoObjetivo) {
   let tempoAtual = new Date();
-  let tempoFinal = tempoObjetivo - tempoAtual;
-}
-let segundos = Math.floor(tempoFinal / 1000);
-let minutos = Math.floor(segundos / 60);
-let horas = Math.floor(minutos / 60);
-let dias = Math.floor(horas / 24);
-function calculaTempo(tempoObjetivo) {
-  let tempoAtual = nova Date();
   let tempoFinal = tempoObjetivo - tempoAtual;
   let segundos = Math.floor(tempoFinal / 1000);
   let minutos = Math.floor(segundos / 60);
   let horas = Math.floor(minutos / 60);
   let dias = Math.floor(horas / 24);
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+  
   return (
-    dias +
-    " dias " +
-    horas +
-    " horas " +
-    minutos +
-    " minutos " +
-    segundos +
-    " segundos"
-  );
+  dias +
+  " dias " +
+  horas +
+  " horas " +
+  minutos +
+  " minutos " +
+  segundos +
+  " segundos"
+);
 }
-segundos %= 60;
-minutos %= 60;
-horas %= 24;
- 
-//
